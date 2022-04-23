1074524315d3693e105c07be13118d66118e9be4
Saksham Mathur <saksham.mathur@trackolap.com>
2022-04-23 14:32:58 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sakshammathur25.MyDiary',
+  appName: 'my_diary_2.0',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
